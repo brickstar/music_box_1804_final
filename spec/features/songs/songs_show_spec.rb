78acf845dd4526cda78e1bec032cb728c5b5d24a
brickstar <brickstar@gmail.com>
2018-07-31 10:15:35 -0600

    user story 7

--- spec/features/songs/songs_show_spec.rb
+++ spec/features/songs/songs_show_spec.rb
@@ -9,4 +9,18 @@ describe 'visitor visits song show' do
 
     expect(page).to have_content("Rating: #{song.rating}")
   end
+
+  it 'should see all genres associated with song and not see unassociated genres' do
+    artist = Artist.create!(name: 'poly')
+    song = Song.create!(title: 'light', length: 1, play_count: 1, rating: 1, artist: artist)
+    genre1 = song.genres.create!(name: 'psych rock')
+    genre2 = song.genres.create!(name: 'punk')
+    genre3 = Genre.create!(name: 'jazz')
+
+    visit song_path(song.slug)
+
+    expect(page).to have_content(genre1.name)
+    expect(page).to have_content(genre2.name)
+    expect(page).to_not have_content(genre3.name)
+  end
 end
