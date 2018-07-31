4cca55a8c5b8b6c3dad4e28d5c062d8ed97e1abb
brickstar <brickstar@gmail.com>
2018-07-31 10:26:28 -0600

    highest and lowest rated song methods

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -7,4 +7,12 @@ class Genre < ApplicationRecord
   def average_song_rating
     songs.average(:rating)
   end
+
+  def highest_rated_song
+    songs.order(rating: :desc).limit(1)[0]
+  end
+
+  def lowest_rated_song
+    songs.order(rating: :asc).limit(1)[0]
+  end
 end
