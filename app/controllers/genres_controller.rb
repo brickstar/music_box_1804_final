33034c49f4ef8bf3e9ffb8056b6be90bf7d4d5e5
brickstar <brickstar@gmail.com>
2018-07-31 09:38:53 -0600

    admin genre create

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,6 +2,23 @@ class GenresController < ApplicationController
 
   def index
     @genres = Genre.all
+    @new_genre = Genre.new
   end
 
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:success] = 'you created a genre!'
+      redirect_to genres_path
+    else
+      flash[:error] = 'your creation did not work'
+      redirect_to genres_path
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
