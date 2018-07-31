a029b9966275092eb1ffa36cbb3f4e049b0ba6d6
brickstar <brickstar@gmail.com>
2018-07-31 09:42:05 -0600

    visitor cannot see create form

--- spec/features/genres/visitor_genre_index_spec.rb
+++ spec/features/genres/visitor_genre_index_spec.rb
@@ -11,4 +11,13 @@ describe 'visitor visits genre index' do
     expect(page).to have_content(genre1.name)
     expect(page).to have_content(genre2.name)
   end
+
+  it 'should not see form to create genre' do
+
+    visit genres_path
+
+    expect(page).to_not have_field('genre[name]')
+    expect(page).to_not have_button('Create Genre')
+  end
+
 end
